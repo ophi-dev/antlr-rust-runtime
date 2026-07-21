ae40a2a6611c93498aa1b48608cc4eed0e82c860
Konstantin Vyatkin <tino@vtkn.io>
2026-07-21 23:17:19 +0200

    test(codegen): port scope parsing cases

--- src/bin_support/embedded.rs
+++ src/bin_support/embedded.rs
@@ -1359,6 +1359,69 @@ mod tests {
         );
     }
 
+    mod upstream_scope_parsing {
+        use super::*;
+
+        const CASES: &[(&str, &str)] = &[
+            ("", ""),
+            (" ", ""),
+            ("int i", "i:int"),
+            ("int[] i, int j[]", "i:int[], j:int []"),
+            ("Map<A,B>[] i, int j[]", "i:Map<A,B>[], j:int []"),
+            ("Map<A,List<B>>[] i", "i:Map<A,List<B>>[]"),
+            (
+                "int i = 34+a[3], int j[] = new int[34]",
+                "i:int=34+a[3], j:int []=new int[34]",
+            ),
+            ("char *[3] foo = {1,2,3}", "foo:char *[3]={1,2,3}"),
+            ("String[] headers", "headers:String[]"),
+            ("std::vector<std::string> x", "x:std::vector<std::string>"),
+            ("i", "i"),
+            ("i,j", "i, j"),
+            ("i\t,j, k", "i, j, k"),
+            ("x: int", "x:int"),
+            ("x :int", "x:int"),
+            ("x:int", "x:int"),
+            ("x:int=3", "x:int=3"),
+            (
+                "r:Rectangle=Rectangle(fromLength: 6, fromBreadth: 12)",
+                "r:Rectangle=Rectangle(fromLength: 6, fromBreadth: 12)",
+            ),
+            ("p:pointer to int", "p:pointer to int"),
+            ("a: array[3] of int", "a:array[3] of int"),
+            ("a \t:\tfunc(array[3] of int)", "a:func(array[3] of int)"),
+            ("x:int, y:float", "x:int, y:float"),
+            (
+                "x:T?, f:func(array[3] of int), y:int",
+                "x:T?, f:func(array[3] of int), y:int",
+            ),
+            ("float64 x = 3", "x:float64=3"),
+            ("map[string]int x", "x:map[string]int"),
+        ];
+
+        #[test]
+        fn argument_declarations_match_java() {
+            for &(input, expected) in CASES {
+                let actual = parse_scope_decls(input)
+                    .into_iter()
+                    .map(render)
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                assert_eq!(actual, expected, "input {input:?}");
+            }
+        }
+
+        fn render(declaration: ScopeDecl) -> String {
+            let ty = declaration
+                .ty
+                .map_or_else(String::new, |ty| format!(":{ty}"));
+            let initializer = declaration
+                .initializer
+                .map_or_else(String::new, |initializer| format!("={initializer}"));
+            format!("{}{ty}{initializer}", declaration.name)
+        }
+    }
+
     #[test]
     fn translates_attr_and_rule_reads() {
         let source = "grammar T;\n\
